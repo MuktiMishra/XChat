7008a7a530e8f2f64590f1973270d56860cf654b
MuktiMishra <muktimishra0495@gmail.com>
2026-02-12 22:26:12 +0530

    frontend fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <div>
         <BrowserRouter>
             <Routes>
-                <Route path="/home" element={<Home />} />
+                <Route path="/" element={<Home />} />
                 <Route path="/create" element={<Create />} /> 
                 <Route path="/chat/:roomId" element={<Chat />} /> 
             </Routes>        
